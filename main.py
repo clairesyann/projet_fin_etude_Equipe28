9a66df8ba13330d48c4680131799baf360a4a59d
clairesyann <constance.garbado@orange.fr>
2023-01-26 12:41:27 +0100

    Toutes les fonctions

--- main.py
+++ main.py
@@ -1,80 +1,53 @@
 import random
+import math
+from collections import defaultdict
 
 class RandomMap:
-    @staticmethod
-    def generate_random_map(n, m):
+    @classmethod
+    def generate_random_map(cls, n, m):
         if n <= 0 or m <= 0:
             return None
         rand = random.Random()
         map = {}
         for i in range(n):
-            key = "liste_"+ str(rand.randint(0, n))
-            list = []
+            key = "liste_{}".format(rand.randint(0, n))
+            lst = []
             for j in range(m):
-                list.append(rand.random())
-            map[key] = list
+                lst.append(rand.random())
+            map[key] = lst
         return map
 
 class MaxValue:
     @staticmethod
     def find_max_values(map):
-        max_value = float('-inf')
-        for key, values in map.items():
-            if values:
-                for value in values:
-                    if value > max_value:
-                        max_value = value
-        return max_value
+        return max(val for vals in map.values() for val in vals)
 
 class MinValue:
     @staticmethod
     def find_min_values(map):
-        min_value = float('inf')
-        for key, values in map.items():
-            if values:
-                for value in values:
-                    if value < min_value:
-                        min_value = value
-        return min_value
+        return min(val for vals in map.values() for val in vals)
 
-
-import math
 
 class StandardDeviation:
     @staticmethod
     def find_standard_deviation_values(map):
         if not map:
             return None
-        count = 0
-        sum_values = 0
-        for key, values in map.items():
-            if values:
-                for value in values:
-                    sum_values += value
-                    count += 1
+        count = sum(len(vals) for vals in map.values())
         if count == 0:
             return None
-        mean = sum_values / count
-        temp = 0
-        for key, values in map.items():
-            if values:
-                for value in values:
-                    temp += (mean - value) ** 2
-        return math.sqrt(temp / (count - 1))
+        mean = MeanValue.find_mean_values(map)
+        deviation_squared_sum = sum((val - mean) ** 2 for vals in map.values() for val in vals)
+        return math.sqrt(deviation_squared_sum / (count - 1))
 
 class MeanValue:
     @staticmethod
     def find_mean_values(map):
-        count = 0
-        sum_values = 0
-        for key, values in map.items():
-            if values:
-                for value in values:
-                    sum_values += value
-                    count += 1
+        count = sum(len(vals) for vals in map.values())
         if count == 0:
             return None
-        return sum_values / count
+        return sum(val for vals in map.values() for val in vals) / count
+
 class Main:
     def main(args):
         print("Hello world!")
