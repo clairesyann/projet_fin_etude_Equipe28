4332dd2280d325125db9b5c5a7197e7aa8925c77
clairesyann <constance.garbado@orange.fr>
2023-03-12 15:04:11 +0100

    map.py la map est directement convertie en JSON

---
+++ map.py
@@ -0,0 +1,33 @@
+import random
+import json
+
+
+class RandomMap:
+    @classmethod
+    def generate_random_map(cls, n, m):
+        if n <= 0 or 0 >= m:
+            return None
+        rand = random.Random()
+        map = {}
+        for i in range(n):
+            key = "liste_{}".format(rand.randint(0, n))
+            lst = []
+            for j in range(m):
+                lst.append(rand.random())
+            map[key] = lst
+        return map
+
+    def convert_to_json(random_map):
+        return json.dumps(random_map)
+
+
+class Main:
+    def main(args):
+        random_map = RandomMap.generate_random_map(10001, 20)
+
+        json_data = RandomMap.convert_to_json(random_map)
+        print(json_data)
+
+
+if __name__ == "__main__":
+    Main.main(None)
