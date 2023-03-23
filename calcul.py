6b7439e3d333b087affe4bccdeaba51b10c9d50e
clairesyann <constance.garbado@orange.fr>
2023-03-23 21:16:24 +0100

    CODE FONCTIONNEL

--- calcul.py
+++ calcul.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
 # a mettre dans /usr/lib/cgi-bin
-#from art import *
-#Art=text2art("",font='block',chr_ignore=True)
-#print('Content-Type: text/plain')
-#print(Art)
+from art import *
+Art=text2art("",font='block',chr_ignore=True)
+print('Content-Type: text/plain')
+print(Art)
 
 import math
 import json
 import os
+import cgi
+import sys
+
 
 class Readfile:
     @staticmethod
+
     def read_map_from_file(filename):
         with open(filename, "r") as file:
             content = file.read()
             random_map = json.loads(content)
         return random_map
 
+    def write_results_to_file(resultats):
+        directory = "/usr/lib/cgi-bin/"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        with open(os.path.join(directory, "calcul_result.txt"), "w") as file:
+            json.dump(resultats, file)
+
     def convert_to_json(resultats):
         return json.dumps(resultats)
 
-    def write_results_to_file(resultats):
-        directory = '/var/www/your_domain/'
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        with open("calcul_result.txt", "w") as file:
-            json.dump(resultats, file)
+
+
 class MaxValue:
     @staticmethod
     def find_max_values(map):
@@ -57,29 +64,31 @@ class MeanValue:
             return None
         return sum(val for vals in map.values() for val in vals) / count
 
+
+
 class Main:
     def main(args):
-        random_map = Readfile.read_map_from_file("map.txt")
+
+
+        map_file = "/usr/lib/cgi-bin/map.txt"
+        # Vérifier si le fichier map.txt existe
+        if not os.path.isfile(map_file):
+            print("Le fichier map.txt n'existe pas dans /usr/lib/cgi-bin")
+            return
+
+        random_map = Readfile.read_map_from_file(map_file)
+
 
         max_value = MaxValue.find_max_values(random_map)
-        #print(max_value)
-
         min_value = MinValue.find_min_values(random_map)
-        #print(min_value)
-
         mean_value = MeanValue.find_mean_values(random_map)
-        #print(mean_value)
-
         standard_deviation = StandardDeviation.find_standard_deviation_values(random_map)
-        #print(standard_deviation)
 
         resultats = [max_value, min_value, mean_value, standard_deviation]
-        print(resultats)
-        json_data = Readfile.convert_to_json(resultats)
-        print(json_data)
 
         Readfile.write_results_to_file(resultats)
-
+        json_data = Readfile.convert_to_json(resultats)
+        print(json_data)
 
 if __name__ == "__main__":
     Main.main(None)
