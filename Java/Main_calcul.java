40a282f50c3de0d1285dc90fc0045d74814aed3a
clairesyann <constance.garbado@orange.fr>
2023-03-27 12:16:29 +0200

    Code JAVA fonctionnel

---
+++ Java/Main_calcul.java
@@ -0,0 +1,156 @@
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class Main_calcul {
+     private static void saveResultsToFile(String filename, double max, double min, double mean, double stdDev) {
+        try {
+            File file = new File(filename);
+            file.createNewFile();
+            java.io.FileWriter writer = new java.io.FileWriter(file);
+            writer.write("Maximum value: " + max + "\n");
+            writer.write("Minimum value: " + min + "\n");
+            writer.write("Mean value: " + mean + "\n");
+            writer.write("Standard deviation: " + stdDev + "\n");
+            writer.close();
+            System.out.println("Results saved to file " + filename);
+        } catch (IOException e) {
+            System.out.println("Error saving results to file " + filename + ": " + e.getMessage());
+        }
+    }
+
+
+    public static void main(String[] args) {
+        // read map from file
+        Map<String, List<Double>> map = readMapFromFile("map.txt");
+
+        //System.out.println("Map: " + map);
+
+        // compute and print statistics
+        double max = MaxValue.findMaxValues(map);
+        double min = MinValue.findMinValues(map);
+        double mean = MeanValue.findMeanValues(map);
+        double stdDev = StandardDeviation.findStandardDeviationValues(map);
+
+        //System.out.println("Maximum value: " + max);
+        //System.out.println("Minimum value: " + min);
+        //System.out.println("Mean value: " + mean);
+        //System.out.println("Standard deviation: " + stdDev);
+        saveResultsToFile("result_java.txt", max, min, mean, stdDev);
+    }
+
+    private static Map<String, List<Double>> readMapFromFile(String filename) {
+        Map<String, List<Double>> map =  new HashMap<>();
+        try {
+            File file = new File(filename);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(" ");
+                String key = parts[0];
+                List<Double> values = new ArrayList<>();
+                for (int i = 1; i < parts.length; i++) {
+                    values.add(Double.parseDouble(parts[i]));
+                }
+                map.put(key, values);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filename);
+        }
+        return map;
+    }
+}
+
+
+public class MaxValue {
+    public static Double findMaxValues(Map<String, List<Double>> map) {
+        if (map == null || map.isEmpty()) {
+            return null;
+        }
+        double max = Double.MIN_VALUE;
+        for (Map.Entry<String, List<Double>> entry : map.entrySet()) {
+            List<Double> list = entry.getValue();
+            if(list != null && !list.isEmpty()){
+                for(double value : list) {
+                    if(value > max)
+                        max = value;
+                }
+            }
+        }
+        return max;
+    }
+}
+
+
+public class MeanValue {
+    public static Double findMeanValues(Map<String, List<Double>> map) {
+        int count = 0;
+        double sum = 0;
+        for (Map.Entry<String, List<Double>> entry : map.entrySet()) {
+            List<Double> list = entry.getValue();
+            if(list != null && !list.isEmpty()){
+                for(double value : list) {
+                    sum += value;
+                    count++;
+                }
+            }
+        }
+        if(count==0) return null;
+        return sum / count;
+    }
+}
+
+public class MinValue {
+    public static Double findMinValues(Map<String, List<Double>> map) {
+        double min = Double.MAX_VALUE;
+        for (var entry : map.entrySet()) {
+            List<Double> list = entry.getValue();
+            if(list != null && !list.isEmpty()){
+                for(double value : list) {
+                    if(value < min)
+                        min = value;
+                }
+            }
+        }
+        return min;
+    }
+}
+
+
+public class StandardDeviation {
+    public static Double findStandardDeviationValues(Map<String, List<Double>> map) {
+        if(map == null || map.isEmpty()) return null;
+        int count = 0;
+        double sum = 0;
+        for (Map.Entry<String, List<Double>> entry : map.entrySet()) {
+            List<Double> list = entry.getValue();
+            if(list != null && !list.isEmpty()){
+                for(double value : list) {
+                    sum += value;
+                    count++;
+                }
+            }
+        }
+        if(count==0) return null;
+        double mean = sum / count;
+        double temp = 0;
+        for (Map.Entry<String, List<Double>> entry : map.entrySet()) {
+            List<Double> list = entry.getValue();
+            if(list != null && !list.isEmpty()){
+                for(double value : list) {
+                    temp += (mean-value)*(mean-value);
+                }
+            }
+        }
+        return Math.sqrt(temp/(count-1));
+    }
+}
